a2c4d56db21863d79944d744cfd1bc2b20305f46
Mateus Coelho <mat_de_souza@hotmail.com>
2024-05-26 18:13:19 -0400

    Campo "Doenças adicionado.

--- App.js
+++ App.js
@@ -3,79 +3,144 @@ import { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Picker } from '@react-native-picker/picker';
-import { StyleSheet, Text, View, Image, Pressable, TextInput, ScrollView } from 'react-native';
+import Checkbox from './components/Checkbox';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Pressable,
+  TextInput,
+  ScrollView,
+} from 'react-native';
 
 // Telas
 function Tela_Principal({ navigation }) {
   return (
     <View style={styles.container}>
-      <StatusBar style="auto"/>
+      <StatusBar style="auto" />
 
       {/* Logo view */}
-      <View style={{flex: 0.85,
-      marginTop: 180,
-      alignItems: 'center'}}>
+      <View style={{ flex: 0.85, marginTop: 180, alignItems: 'center' }}>
         <Image
           style={styles.tinyLogo}
           source={{
-            uri:'https://reactnative.dev/img/tiny_logo.png',
+            uri: 'https://reactnative.dev/img/tiny_logo.png',
           }}
         />
-        <Text style={{fontSize: 32, color: 'black'}}>
-          {'ezProtocol'}
-        </Text>
+        <Text style={{ fontSize: 32, color: 'black' }}>ezProtocol</Text>
       </View>
 
       {/* Button view */}
-      <View style={{flex: 0.15}}>
-      <Pressable style={styles.button}
-        onPress={() => {
-          console.log('Navegando para a tela "Solicitar Exames"...');
-          navigation.navigate("Solicitar Exames")
-        }}>
-        <Text style={styles.buttonText}>
-          {'Solicitar Exames'}
-        </Text>
-      </Pressable>
+      <View style={{ flex: 0.15 }}>
+        <Pressable
+          style={styles.button}
+          onPress={() => {
+            console.log('Navegando para a tela "Solicitar Exames"...');
+            navigation.navigate('Solicitar Exames');
+          }}>
+          <Text style={styles.buttonText}>Solicitar Exames</Text>
+        </Pressable>
       </View>
     </View>
-  )
+  );
 }
 function Tela_SolicitarExames({ navigation }) {
   const [genero, setGenero] = useState(); // Armazena o valor do Picker "Gênero"
-  const handleValueChange=(itemValue, itemIndex) => setGenero(itemValue)  // Altera o valor do Picker "Gênero"
-  return (    
+  const handleValueChange = (itemValue, itemIndex) => setGenero(itemValue); // Altera o valor do Picker "Gênero"
+  const diseases = [
+    { id: 'Cardiopatia hipertensiva' },
+    { id: 'Diabetes mellitus' },
+    { id: 'Arritmias cardíacas' },
+    { id: 'Cardiopatia hipertensiva' },
+    { id: 'Diabetes mellitus' },
+    { id: 'Arritmias cardíacas' },
+    { id: 'Cardiopatia hipertensiva' },
+    { id: 'Diabetes mellitus' },
+    { id: 'Arritmias cardíacas' },
+    { id: 'Cardiopatia hipertensiva' },
+    { id: 'Diabetes mellitus' },
+    { id: 'Arritmias cardíacas' },
+    { id: 'Cardiopatia hipertensiva' },
+    { id: 'Diabetes mellitus' },
+    { id: 'Arritmias cardíacas' },
+    { id: 'Cardiopatia hipertensiva' },
+    { id: 'Diabetes mellitus' },
+    { id: 'Arritmias cardíacas' },
+  ];
+  const [selectedDiseases, setSelectedDiseases] = useState([]);
+  return (
     <View style={styles.container}>
-      <StatusBar style="auto" translucent={false} hidden={false}/>
+      <StatusBar style="auto" translucent={false} hidden={false} />
 
       {/* Título */}
-      <View style={{flex: 0.08, width: '100%', alignItems: 'center', alignContent: 'center', backgroundColor: '#a2a2a2'}}>
-        <Text style={{fontSize: 20, marginTop: 15}}>{'Informações do Paciente'}</Text>
+      <View
+        style={{
+          flex: 0.08,
+          width: '100%',
+          alignItems: 'center',
+          alignContent: 'center',
+          backgroundColor: '#a2a2a2',
+        }}>
+        <Text style={{ fontSize: 20, marginTop: 18 }}>
+          Informações do Paciente
+        </Text>
       </View>
 
       {/* Campos */}
-      <View style={{flex: 0.92, width: '100%', alignItems: 'center', backgroundColor: 'white'}}>
-        <ScrollView style={{width: '100%'}}>
-
+      <View
+        style={{
+          flex: 0.92,
+          width: '100%',
+          alignItems: 'center',
+          backgroundColor: 'white',
+        }}>
+        <ScrollView style={{ width: '100%' }}>
           {/* Idade */}
           <View>
-            <Text style={{marginLeft: 12, marginTop: 18}}>{'Idade'}</Text>
-            <TextInput style={styles.input} placeholder="18" inputMode="numeric"/>
+            <Text style={{ marginLeft: 12, marginTop: 18 }}>Idade</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="18"
+              inputMode="numeric"
+            />
           </View>
-          
+
           {/* Gênero */}
           <View>
-            <Text style={{marginLeft: 12}}>{'Gênero'}</Text>
-            <Picker style={{marginLeft: 12, marginTop: 10, width: '93%', borderColor: 'black'}} selectedValue={genero} onValueChange={handleValueChange} mode='dropdown'>
+            <Text style={{ marginLeft: 12, marginTop: 18 }}>Gênero</Text>
+            <Picker
+              style={{
+                marginLeft: 12,
+                marginTop: 12,
+                width: '93%',
+                borderColor: 'black',
+              }}
+              selectedValue={genero}
+              onValueChange={handleValueChange}
+              mode="dropdown">
               <Picker.Item label="Masculino" value="m" />
               <Picker.Item label="Feminino" value="f" />
             </Picker>
-          </View>         
+          </View>
 
-        </ScrollView>      
+          {/* Doenças */}
+          <View>
+            <Text style={{ marginLeft: 12, marginTop: 18 }}>Doenças</Text>
+            <ScrollView style={{ width: '90%', height: 200, marginTop: 12 }}>
+              <Checkbox
+                id="diseases"
+                options={diseases}
+                selected={selectedDiseases}
+                setSelected={setSelectedDiseases}
+                multiple
+              />
+            </ScrollView>
+          </View>
+        </ScrollView>
       </View>
     </View>
-  )
+  );
 }
 
 // Navegação
@@ -83,11 +148,12 @@ export default function App() {
   const Stack = createNativeStackNavigator(); // Controla a navegação entre telas
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-
-        <Stack.Screen name="Tela Principal" component={Tela_Principal}/>
-        <Stack.Screen name="Solicitar Exames" component={Tela_SolicitarExames}/>
-
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        {/* <Stack.Screen name="Tela Principal" component={Tela_Principal}/> */}
+        <Stack.Screen
+          name="Solicitar Exames"
+          component={Tela_SolicitarExames}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -101,7 +167,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  
+
   tinyLogo: {
     width: 50,
     height: 50,
@@ -123,7 +189,8 @@ const styles = StyleSheet.create({
 
   input: {
     height: 40,
-    margin: 12,
+    marginHorizontal: 12,
+    marginTop: 12,
     borderWidth: 1,
     padding: 10,
   },
